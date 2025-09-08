bd17ed0897a0318827a14dc96cbfb89c4b7e9297
Yushi Yang <yangyushi1992@icloud.com>
2025-09-08 12:23:29 +0800

    fix llama config

--- lua/plugins/llama.lua
+++ lua/plugins/llama.lua
@@ -1,14 +1,9 @@
 return {
     'ggml-org/llama.vim',
-    opts = {
-        auto_fim = true,
-        show_info = 1,
-        llama_enabled = false
-    },
     keys = {
         {
             "<F3>",
-            function() vim.fn["llama#toggle"]() end,
+            "<cmd>LlamaToggle<cr>",
             mode = { "n", "i" },
             desc = "Toggle llama.vim",
         },
